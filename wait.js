5072802cf1a815ba1af68048edec8e39b52bbf6f
merroth <sj.emil@gmail.com>
2016-08-29 17:34:18 +0200

    Add files via upload

--- wait.js
+++ wait.js
@@ -1,23 +1,71 @@
 ////Classes
-//This is the "Wait" class.
-//We use class instead of namespace because that allows the creation of individual queues.
+/**
+ * We use class instead of namespace because that allows the creation of individual queues.
+ * @class Wait
+ */
 var Wait = (function () {
+    /**
+     * Creates an instance of Wait.
+     * @returns {Wait}
+     */
     function Wait() {
-        //Switch for displaying errors. Default is true
+        /**
+         * Switch for handling events synchronously.
+         * If synchronousHandling is false then we will always wait at least 'checkTime' milliseconds between each execution.
+         */
+        this.synchronousHandling = true;
+        /**
+         * Shorthand for 'setSynchronousHandling'.
+         */
+        this.sync = this.setSynchronousHandling;
+        /**
+         * Switch for displaying errors.
+         * Default is true.
+         */
         this.logErrors = true;
-        //Shorthand for displayErrors
+        /**
+         * Shorthand for 'displayErrors'.
+         */
         this.err = this.displayErrors;
-        //Minimum ms between checks
-        //Defaults to 2 checks per standard 60 fps frame
+        /**
+         * Minimum ms between checks.
+         * Defaults to 2 checks per standard 60 fps frame.
+         */
         this.checkTime = 8;
-        //Shorthand for setCheckTime
+        /**
+         * Shorthand for 'setCheckTime'.
+         */
         this.t = this.setCheckTime;
-        //The queue of tasks, a list of tasks following the "Iqueue" format.
+        /**
+         * The queue of tasks.
+         * A list of tasks following the "Iqueue" format.
+         */
         this.queue = [];
-        //Shorthand for register
+        /**
+         * Shorthand for register.
+         */
         this.r = this.register;
     }
-    //Setter for logErrors
+    /**
+     * Setter for 'synchronousHandling'.
+     * Takes a boolean argument.
+     * @param {boolean} v
+     * @returns {Wait}
+     */
+    Wait.prototype.setSynchronousHandling = function (v) {
+        if (typeof v != "boolean") {
+            this.log("'setSynchronousHandling' only takes type boolean arguments, argument of type '" + typeof v + "' is not allowed");
+            return this;
+        }
+        this.synchronousHandling = v;
+        return this;
+    };
+    /**
+     * Setter for 'logErrors'.
+     * Takes a boolean argument.
+     * @param {boolean} v
+     * @returns {Wait}
+     */
     Wait.prototype.displayErrors = function (v) {
         if (typeof v != "boolean") {
             this.log("'displayErrors' only takes type boolean arguments, argument of type '" + typeof v + "' is not allowed");
@@ -26,18 +74,29 @@ var Wait = (function () {
         this.logErrors = v;
         return this;
     };
-    //Log function
+    /**
+     * Log function.
+     * Logs warning messages to the console.
+     */
     Wait.prototype.log = function (message) {
         if (message === void 0) { message = ""; }
         if (this.logErrors === true) {
             console.warn(message);
         }
     };
-    //Getter for checkTime
+    /**
+     * Getter for 'checkTime'.
+     * @returns {number}
+     */
     Wait.prototype.getCheckTime = function () {
         return this.checkTime;
     };
-    //Setter for checkTime
+    /**
+     * Setter for 'checkTime'.
+     * Takes a millisecond argument.
+     * @param {number} [time=this.checkTime]
+     * @returns {Wait}
+     */
     Wait.prototype.setCheckTime = function (time) {
         if (time === void 0) { time = this.checkTime; }
         //Time must be a number
@@ -55,8 +114,13 @@ var Wait = (function () {
         //Return this for chaining
         return this;
     };
-    //The main function for this class. Wait takes a queue item and tests the condition.
-    //If the condition returns true then the callback is called and the next item in the queue (if any) is called.
+    /**
+     * The main function for this class.
+     * Wait takes a queue item and tests the condition.
+     * If the condition returns true then the callback is called and the next item in the queue (if any) is called.
+     * @private
+     * @param {Iqueue} queueItem
+     */
     Wait.prototype.wait = function (queueItem) {
         var _this = this;
         //Check current queueItem condition
@@ -67,14 +131,25 @@ var Wait = (function () {
             this.queue.splice(0, 1);
             //If the queue contains more items
             if (this.queue.length > 0) {
-                //Fire next event synchronously to  accomodate instantly passed conditions
-                this.wait(this.queue[0]);
+                //Check for synchronous handling of "queueItems"
+                if (this.synchronousHandling === true) {
+                    //Fire next event synchronously to accomodate instantly passed conditions
+                    this.wait(this.queue[0]);
+                }
+                else {
+                    //Make sure only one instance of the queue is running by clearing our timeout
+                    clearTimeout(this.timeoutHandle);
+                    //Set a new timeout to test the next "queueItem" in 'checkTime' milliseconds
+                    this.timeoutHandle = setTimeout(function () {
+                        return _this.wait(_this.queue[0]);
+                    }, this.checkTime);
+                }
             }
         }
         else {
             //Make sure only one instance of the queue is running by clearing our timeout
             clearTimeout(this.timeoutHandle);
-            //Set a new timeout to test the next queueItem in checkTime milliseconds
+            //Set a new timeout to test the next "queueItem" in 'checkTime' milliseconds
             this.timeoutHandle = setTimeout(function () {
                 return _this.wait(queueItem);
             }, this.checkTime);
@@ -102,6 +177,11 @@ var Wait = (function () {
                 return this;
             }
         }
+        //Test that condition returns a boolean value
+        if (typeof condition() !== "boolean") {
+            this.log("'register' argument 'condition' must evaluate to type 'boolean', evaluation of type '" + (typeof condition()) + "' is not allowed");
+            return this;
+        }
         //Push the new queueItem to the queue
         this.queue.push({
             condition: condition,
@@ -110,9 +190,12 @@ var Wait = (function () {
         //return this for chaining
         return this;
     };
-    //This starts the execution of the queue.
-    //Notice that this will clear the timeout handle to ensure only one process is running.
-    //Returns true if a queue was started and false if no queue items exists.
+    /**
+     * This starts the execution of the queue.
+     * Notice that this will clear the timeout handle to ensure only one process is running.
+     * Returns true if a queue was started and false if no queue items exists.
+     * @returns {Wait}
+     */
     Wait.prototype.start = function () {
         if (this.queue.length > 0) {
             clearTimeout(this.timeoutHandle);
@@ -122,6 +205,12 @@ var Wait = (function () {
         this.log("'start' cannot activate on an empty queue. 'register' a task first");
         return this;
     };
+    /**
+     * Pauses the execution of the queue.
+     * This also clears the queue if the "clearQueue" parameter is set to true.
+     * @param {boolean} [clearQueue=false]
+     * @returns {Wait}
+     */
     Wait.prototype.stop = function (clearQueue) {
         if (clearQueue === void 0) { clearQueue = false; }
         clearTimeout(this.timeoutHandle);
--- wait.ts
+++ wait.ts
@@ -4,16 +4,56 @@ interface Iqueue {
 	//callback must be a function, but may return anything
 	callback: () => any
 	//condition must be a function and must return true or false
-	condition: () => boolean,
+	condition: () => boolean
 }
 ////Classes
-//This is the "Wait" class.
-//We use class instead of namespace because that allows the creation of individual queues.
+/**
+ * We use class instead of namespace because that allows the creation of individual queues.
+ * @class Wait
+ */
 class Wait {
-	//Switch for displaying errors. Default is true
+	/**
+	 * Creates an instance of Wait.
+	 * @returns {Wait}
+	 */
+	constructor () {
+		
+	}
+	/**
+	 * Switch for handling events synchronously.
+	 * If synchronousHandling is false then we will always wait at least 'checkTime' milliseconds between each execution.
+	 */
+	private synchronousHandling: boolean = true;
+	/**
+	 * Setter for 'synchronousHandling'.
+	 * Takes a boolean argument.
+	 * @param {boolean} v
+	 * @returns {Wait}
+	 */
+	public setSynchronousHandling(v: boolean): Wait {
+		if (typeof v != "boolean") {
+			this.log("'setSynchronousHandling' only takes type boolean arguments, argument of type '" + typeof v + "' is not allowed");
+			return this;
+		}
+		this.synchronousHandling = v;
+		return this;
+	}
+	/**
+	 * Shorthand for 'setSynchronousHandling'.
+	 */
+	public sync = this.setSynchronousHandling;
+	/**
+	 * Switch for displaying errors.
+	 * Default is true.
+	 */
 	private logErrors: boolean = true;
-	//Setter for logErrors
-	public displayErrors(v: boolean) {
+	/**
+	 * Setter for 'logErrors'.
+	 * Takes a boolean argument.
+	 * @param {boolean} v
+	 * @returns {Wait}
+	 */
+	public displayErrors(v: boolean): Wait {
 		if (typeof v != "boolean") {
 			this.log("'displayErrors' only takes type boolean arguments, argument of type '" + typeof v + "' is not allowed");
 			return this;
@@ -21,22 +61,37 @@ class Wait {
 		this.logErrors = v;
 		return this;
 	}
-	//Shorthand for displayErrors
+	/**
+	 * Shorthand for 'displayErrors'.
+	 */
 	public err = this.displayErrors;
-	//Log function
+	/**
+	 * Log function.
+	 * Logs warning messages to the console.
+	 */
 	private log(message: string = "") {
 		if (this.logErrors === true) {
 			console.warn(message);
 		}
 	}
-	//Minimum ms between checks
-	//Defaults to 2 checks per standard 60 fps frame
+	/**
+	 * Minimum ms between checks.
+	 * Defaults to 2 checks per standard 60 fps frame.
+	 */
 	private checkTime: number = 8;
-	//Getter for checkTime
+	/**
+	 * Getter for 'checkTime'.
+	 * @returns {number}
+	 */
 	public getCheckTime() {
 		return this.checkTime;
 	}
-	//Setter for checkTime
+	/**
+	 * Setter for 'checkTime'.
+	 * Takes a millisecond argument.
+	 * @param {number} [time=this.checkTime]
+	 * @returns {Wait}
+	 */
 	public setCheckTime(time: number = this.checkTime): Wait {
 		//Time must be a number
 		if (typeof time != "number") {
@@ -53,14 +108,26 @@ class Wait {
 		//Return this for chaining
 		return this;
 	}
-	//Shorthand for setCheckTime
+	/**
+	 * Shorthand for 'setCheckTime'.
+	 */
 	public t = this.setCheckTime;
-	//The queue of tasks, a list of tasks following the "Iqueue" format.
+	/**
+	 * The queue of tasks.
+	 * A list of tasks following the "Iqueue" format.
+	 */
 	private queue: Iqueue[] = [];
-	//The timeout handle ensures that we can stop the queue at any time.
+	/**
+	 * The timeout handle ensures that we can stop the queue at any time.
+	 */
 	private timeoutHandle: number;
-	//The main function for this class. Wait takes a queue item and tests the condition.
-	//If the condition returns true then the callback is called and the next item in the queue (if any) is called.
+	/**
+	 * The main function for this class.
+	 * Wait takes a queue item and tests the condition.
+	 * If the condition returns true then the callback is called and the next item in the queue (if any) is called.
+	 * @private
+	 * @param {Iqueue} queueItem
+	 */
 	private wait(queueItem: Iqueue) {
 		//Check current queueItem condition
 		if (queueItem.condition()) {
@@ -70,25 +137,57 @@ class Wait {
 			this.queue.splice(0, 1);
 			//If the queue contains more items
 			if (this.queue.length > 0) {
-				//Fire next event synchronously to  accomodate instantly passed conditions
-				this.wait(this.queue[0]);
+				//Check for synchronous handling of "queueItems"
+				if (this.synchronousHandling === true) {
+					//Fire next event synchronously to accomodate instantly passed conditions
+					this.wait(this.queue[0]);
+				} else {
+					//Make sure only one instance of the queue is running by clearing our timeout
+					clearTimeout(this.timeoutHandle);
+					//Set a new timeout to test the next "queueItem" in 'checkTime' milliseconds
+					this.timeoutHandle = setTimeout(() =>
+						this.wait(this.queue[0])
+						, this.checkTime);
+				}
 			}
 		} else {
 			//Make sure only one instance of the queue is running by clearing our timeout
 			clearTimeout(this.timeoutHandle);
-			//Set a new timeout to test the next queueItem in checkTime milliseconds
+			//Set a new timeout to test the next "queueItem" in 'checkTime' milliseconds
 			this.timeoutHandle = setTimeout(() =>
 				this.wait(queueItem)
 				, this.checkTime);
 		}
 	}
-	//Registers queue items for the queue
-	//This function uses the overload method because the condition isn't necessarily important intellisense for the user
-	//Notice that this doesn't start the execution of the queue, it simply adds items to it
+	/**
+	 * Registers queue items for the queue.
+	 * Notice that this doesn't start the execution of the queue, it simply adds items to it.
+	 * Assumes all conditions to have been met immediately.
+	 * @param {() => any} callback
+	 * @returns {Wait}
+	 */
 	register(callback: () => any): Wait;
+	/**
+	 * Registers queue items for the queue.
+	 * Notice that this doesn't start the execution of the queue, it simply adds items to it.
+	 * Uses a boolean as a condition.
+	 * Change the condition variable in a previous step or outside wait queue or simply make it "true" to fire the callback
+	 * @param {() => any} callback
+	 * @param {boolean} condition
+	 * @returns {Wait}
+	 */
 	register(callback: () => any, condition: boolean): Wait;
+	/**
+	 * Registers queue items for the queue.
+	 * Notice that this doesn't start the execution of the queue, it simply adds items to it.
+	 * Define a function as the condition.
+	 * The condition must return a boolean value.
+	 * @param {() => any} callback
+	 * @param {() => boolean} condition
+	 * @returns {Wait}
+	 */
 	register(callback: () => any, condition: () => boolean): Wait;
-	public register(callback: () => any, condition: any = function () { return true; }) {
+	public register(callback: () => any, condition: any = function () { return true; }): Wait {
 		//Fail gracefully if parameters is of the wrong type
 		if (typeof callback != "function") {
 			this.log("'register' argument 'callback' must be of type 'function'");
@@ -107,6 +206,11 @@ class Wait {
 				return this;
 			}
 		}
+		//Test that condition returns a boolean value
+		if (typeof condition() !== "boolean") {
+			this.log("'register' argument 'condition' must evaluate to type 'boolean', evaluation of type '" + (typeof condition()) + "' is not allowed");
+			return this;
+		}
 		//Push the new queueItem to the queue
 		this.queue.push({
 			condition: condition,
@@ -115,11 +219,16 @@ class Wait {
 		//return this for chaining
 		return this;
 	}
-	//Shorthand for register
+	/**
+	 * Shorthand for register.
+	 */
 	public r = this.register;
-	//This starts the execution of the queue.
-	//Notice that this will clear the timeout handle to ensure only one process is running.
-	//Returns true if a queue was started and false if no queue items exists.
+	/**
+	 * This starts the execution of the queue.
+	 * Notice that this will clear the timeout handle to ensure only one process is running.
+	 * Returns true if a queue was started and false if no queue items exists.
+	 * @returns {Wait}
+	 */
 	public start(): Wait {
 		if (this.queue.length > 0) {
 			clearTimeout(this.timeoutHandle);
@@ -129,11 +238,25 @@ class Wait {
 		this.log("'start' cannot activate on an empty queue. 'register' a task first");
 		return this;
 	}
-	//Pauses the execution of the queue.
-	//This also clears the queue if the "clearQueue" parameter is set to true.
+	/**
+	 * Pauses the execution of the queue.
+	 * @returns {Wait}
+	 */
 	stop(): Wait;
+	/**
+	 * Pauses the execution of the queue.
+	 * This also clears the queue if the "clearQueue" parameter is set to true.
+	 * @param {boolean} clearQueue
+	 * @returns {Wait}
+	 */
 	stop(clearQueue: boolean): Wait;
-	public stop(clearQueue: boolean = false) {
+	/**
+	 * Pauses the execution of the queue.
+	 * This also clears the queue if the "clearQueue" parameter is set to true.
+	 * @param {boolean} [clearQueue=false]
+	 * @returns {Wait}
+	 */
+	public stop(clearQueue: boolean = false): Wait {
 		clearTimeout(this.timeoutHandle);
 		if (clearQueue === true) {
 			this.queue = [];
@@ -141,3 +264,4 @@ class Wait {
 		return this;
 	}
 }
+
